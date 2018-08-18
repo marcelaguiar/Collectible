8a5f1ac18a5ab5b76dc235e98a611a6f94690d4e
marcelaguiar <marcelaguiar9@gmail.com>
2018-08-17 22:49:40 -0700

    Set up some basic models

--- collectionsapp/models.py
+++ collectionsapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Collection(models.Model):
+    collection_type_id = models.ForeignKey('CollectionType', on_delete=models.PROTECT)
+
+
+class CollectionType(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class BottleCap(models.Model):
+    manufacturer = models.CharField(max_length=200)
