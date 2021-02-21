3aa35622fd5515c5dd728bd8895ac73090cf9daa
Marcel Aguiar <marcelaguiar9@gmail.com>
2021-02-20 21:14:10 -0800

    Admin panel updates

--- collectionsapp/admin.py
+++ collectionsapp/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import BeverageType, BottleCap, CollectionType, Collection, MethodAcquired, SearchAction
 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+class MyUserAdmin(UserAdmin):
+    ordering = ('-date_joined',)
+    list_display = ('username', 'email', 'date_joined', 'is_staff')
+
+
+# finally replace the default UserAdmin with yours
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
+
 # Register your models here.
 admin.site.register(CollectionType)
 admin.site.register(BeverageType)
