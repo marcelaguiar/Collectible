7837e5cfe0758336b6a976bc08738bd96f6bd920
Marcel Aguiar <marcelaguiar9@gmail.com>
2021-03-16 01:05:00 -0700

    switch get to filter

--- collectionsapp/helpers/json_helper.py
+++ collectionsapp/helpers/json_helper.py
@@ -80,6 +80,6 @@ def add_full_urls(items):
 
 
 def get_all_collection_item_ids(request):
-    data = BottleCap.objects.get(image_thumbnail_tiny__startswith="thumbnails/images/").values('id')
+    data = BottleCap.objects.filter(image_thumbnail_tiny__startswith="thumbnails/images/").values('id')
 
     return JsonResponse(list(data), safe=False)
